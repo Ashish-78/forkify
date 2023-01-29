a07370d4ecce144d92cefdbdeaec6437954846c1
Ashish-78 <ashish05.git@gmail.com>
2023-01-29 20:49:36 +0530

    Added New Features

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,7 +3,11 @@ import icons from '../../img/icons.svg';
 
 export default class View {
   _data;
-
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
